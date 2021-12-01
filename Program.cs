2dc60320b9cd8afd231f9506244776c1154a8c10
shalini Kumarii <bittukumari@gmail.com>
2021-12-01 13:58:18 -0800

    Phone first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Phone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Nokia nokia= new Nokia("First",100,"Cricket","Bzzzzzzzz");
+            nokia.DisplayInfo();
+            Console.WriteLine(nokia.Ring());
+            Console.WriteLine(nokia.Unlock());
+            Console.WriteLine("");
+
+            Galaxy galaxy = new Galaxy("S8",100,"T-mobile","DOO DODOO");
+            galaxy.DisplayInfo();
+            Console.WriteLine(galaxy.Ring());
+            Console.WriteLine(galaxy.Unlock());
+            
+
+        }
+    }
+}
